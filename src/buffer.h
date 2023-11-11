862450687141cb687e104048b56c1a93e1d8724f
ttibsi <ashisbitt@icloud.com>
2023-11-11 22:08:48 +0000

    builds

--- src/buffer.h
+++ src/buffer.h
@@ -16,12 +16,15 @@ struct Buffer {
     bool readonly;
     bool modified;
     std::size_t current_line; // 0-indexed on the current line of lines
+    bool quit_buf = false;
 
     Buffer(Editor *);
     Buffer(Editor *, std::string);
     void init(rawterm::Pos);
     std::string render_status_bar(const std::size_t &, Cursor *);
     void reset_status_bar(rawterm::Pos, Cursor *);
+    void split_lines(const Cursor &);
+    void parse_command(const std::string &);
 };
 
 #endif // BUFFER_H
