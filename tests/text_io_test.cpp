2935c5be01ceeaca7d2351ed95e749db1ce3e5aa
ttibsi <ashisbitt@icloud.com>
2025-01-28 19:03:18 +0000

    test for no newline file

--- tests/text_io_test.cpp
+++ tests/text_io_test.cpp
@@ -6,15 +6,26 @@ boost::ut::suite<"Text IO"> file_io_suite = [] {
     using namespace boost::ut;
 
     "open_file"_test = [] {
-        lines_t expected = {
-            "This is some text", "    here is a newline and a tab", "and another newline"};
+        should("Normal file") = [] {
+            lines_t expected = {
+                "This is some text", "    here is a newline and a tab", "and another newline"};
 
-        opt_lines_t actual = open_file("tests/fixture/test_file_1.txt");
+            opt_lines_t actual = open_file("tests/fixture/test_file_1.txt");
 
-        expect(actual.has_value() == true);
-        expect(actual.value() == expected);
-        expect(actual.value().at(1) == expected.at(1));
+            expect(actual.has_value() == true);
+            expect(actual.value() == expected);
+            expect(actual.value().at(1) == expected.at(1));
+        };
+
+        should("One line, no newlines") = [] {
+            opt_lines_t actual = open_file("tests/fixture/no_newline_file.txt");
+            expect(actual.has_value() == true);
+            expect(actual.value().size() == 1);
+            expect(actual.value().at(0) == "Hello");
+        };
     };
+
+    skip / "write_to_file"_test = [] {};
 
     "lines"_test = [] {
         std::string s = "foo\nbar\r\nbaz";
