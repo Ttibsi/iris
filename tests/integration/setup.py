236f771676c9779075114fef4a0988387c98f38b
ttibsi <ashisbitt@icloud.com>
2025-01-28 18:50:27 +0000

    Add generation of temp file to hecate runner decorator

--- tests/integration/setup.py
+++ tests/integration/setup.py
@@ -1,20 +1,28 @@
+import os
 from collections.abc import Callable
+from typing import Final
 from typing import TypeVar
 
 from hecate import Runner
 
 
 T = TypeVar("T", bound=Callable[[Runner], None])
+CMD_KEY: Final[str] = "\\;"
 
 
 def setup(open_with: str = "") -> Callable[[T], Callable[[], None]]:
+
     def decorator(func: T) -> Callable[[], None]:
         def wrapper() -> None:
+            with open("tests/fixture/temp_file.txt", "w") as f:
+                f.write("Hello world")
+
             dims = {"width": 80, "height": 24}
             with Runner("build/src/iris", open_with, **dims) as r:
                 r.await_text("READ", timeout=2)
                 func(r)
-                r.press("q")
+
+            os.remove("tests/fixture/temp_file.txt")
         return wrapper
     return decorator
 
