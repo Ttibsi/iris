6dbdc0ee2e5affd26ded418cb201539e27175c49
ttibsi <ashisbitt@icloud.com>
2025-03-22 16:48:32 +0000

    add git tag to version

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,23 +1,19 @@
 function(version_setup)
     execute_process(
-        COMMAND git describe
+        COMMAND git describe --tags
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-        OUTPUT_VARIABLE GIT_VERSION
-        ERROR_VARIABLE GIT_DESC_ERROR
-        RESULT_VARIABLE RET
+        OUTPUT_VARIABLE GIT_TAG
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if(NOT (RET EQUAL 0))
-        execute_process(
-            COMMAND git log -1 --format=%h
-            WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-            OUTPUT_VARIABLE GIT_HASH
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
+    execute_process(
+        COMMAND git log -1 --format=%h
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        OUTPUT_VARIABLE GIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
 
-        set(GIT_VERSION "v0.0.0-${GIT_HASH}")
-    endif()
+    set(GIT_VERSION "${GIT_TAG}-${GIT_HASH}")
 
     string(TIMESTAMP COMPILE_DATE "%Y/%m/%d")
     if(CMAKE_BUILD_TYPE STREQUAL "")
