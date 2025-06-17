21af743c1f2b43bdfa2b9182e1edbd806af263d7
Ttibsi <ashisbitt@icloud.com>
2025-06-17 08:34:33 +0000

    cleanup todo

--- src/change.h
+++ src/change.h
@@ -6,16 +6,15 @@
 // Forward declare to prevent circular header includes
 enum class ActionType;
 
-// TODO: Remove optional from payload?
 struct Change {
     const ActionType action;
-    char payload = '\0';
+    char payload;
     const std::optional<unsigned int> line_pos;
     const std::optional<unsigned int> char_pos;
 
-    Change(ActionType a) : action(a) {}
+    Change(ActionType a) : action(a), payload('\0') {}
     Change(ActionType a, unsigned int ln, unsigned int ch)
-        : action(a), line_pos(ln), char_pos(ch) {}
+        : action(a), payload('\0'), line_pos(ln), char_pos(ch) {}
     Change(ActionType a, char c, unsigned int ln, unsigned int ch)
         : action(a), payload(c), line_pos(ln), char_pos(ch) {}
 };
