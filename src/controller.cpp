ef013912fbc24dbcd5db4a6671f3e0c6ad7ed787
Ttibsi <ashisbitt@icloud.com>
2025-07-03 07:32:20 +0000

    rename ls cmd to lb

--- src/controller.cpp
+++ src/controller.cpp
@@ -511,9 +511,9 @@ bool Controller::parse_command() {
     } else if (cmd == ";ping") {
         view.display_message(std::string("pong"), rawterm::Colors::white);
 
-    } else if (cmd == ";sl") {
-        view.display_message(std::string("Did you mean: `ls`"), rawterm::Colors::red);
     } else if (cmd == ";ls") {
+        view.display_message(std::string("Did you mean: `lb`"), rawterm::Colors::red);
+    } else if (cmd == ";lb") {
         display_all_buffers();
 
     } else {
