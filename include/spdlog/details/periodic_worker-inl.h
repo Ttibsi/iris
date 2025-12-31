28b7b7cb1e494f9643629bc7f3446697472cbe8b
ttibsi <ashisbitt@icloud.com>
2025-12-31 13:33:53 +0000

    spdlog head

--- include/spdlog/details/periodic_worker-inl.h
+++ include/spdlog/details/periodic_worker-inl.h
@@ -8,19 +8,19 @@
 #endif
 
 namespace spdlog {
-    namespace details {
+namespace details {
 
-        // stop the worker thread and join it
-        SPDLOG_INLINE periodic_worker::~periodic_worker() {
-            if (worker_thread_.joinable()) {
-                {
-                    std::lock_guard<std::mutex> lock(mutex_);
-                    active_ = false;
-                }
-                cv_.notify_one();
-                worker_thread_.join();
-            }
+// stop the worker thread and join it
+SPDLOG_INLINE periodic_worker::~periodic_worker() {
+    if (worker_thread_.joinable()) {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            active_ = false;
         }
+        cv_.notify_one();
+        worker_thread_.join();
+    }
+}
 
-    }  // namespace details
+}  // namespace details
 }  // namespace spdlog
--- include/spdlog/fwd.h
+++ include/spdlog/fwd.h
@@ -4,15 +4,15 @@
 #pragma once
 
 namespace spdlog {
-    class logger;
-    class formatter;
+class logger;
+class formatter;
 
-    namespace sinks {
-        class sink;
-    }
+namespace sinks {
+class sink;
+}
 
-    namespace level {
-        enum level_enum : int;
-    }
+namespace level {
+enum level_enum : int;
+}
 
 }  // namespace spdlog
