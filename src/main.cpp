d5f3848a5df59e72a17ee39925930371c7159003
Ttibsi <ashisbitt@icloud.com>
2026-06-05 14:23:43 +0000

    Implement handler logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <memory>
 #include <print>
 #include <string>
+// To link stacktrace, we need `-lstdc++exp
+#include <stacktrace>
 
 #include <cli11/CLI11.hpp>
 #include <rawterm/core.h>
@@ -16,6 +19,33 @@
 // TODO: detect if `;e` is opening an already-open file and switch to that buf instead
 // NOTE: Maybe post a message in the command bar too
 // TODO: A way of detecting if the file is already open in another iris instance
+
+void exit_app() {
+    rawterm::exit_alt_screen();
+    rawterm::Cursor::cursor_block();
+}
+
+[[noreturn]] constexpr void handler() noexcept {
+    exit_app();
+
+    std::shared_ptr<spdlog::logger> err_log = spdlog::get("basic_logger");
+    auto log_msg = [&](std::string_view sv) {
+        std::println("{}", sv);
+        err_log->error("{}", sv);
+    };
+
+    const std::string bt = std::to_string(std::stacktrace::current());
+
+    const auto eptr = std::current_exception();
+    if (eptr) {
+        try {
+            std::rethrow_exception(eptr);
+        } catch (const std::exception& e) { log_msg(e.what()); }
+    }
+
+    log_msg(std::format("\n{}", bt));
+    std::exit(-1);
+}
 
 int main(int argc, char* argv[]) {
     CLI::App app {"Iris text editor"};
@@ -50,19 +80,12 @@ int main(int argc, char* argv[]) {
     rawterm::enter_alt_screen();
     rawterm::enable_raw_mode();
 
-    try {
-        std::println("Setup...");
-        Controller c;
-        c.create_view(flags);
-        c.start_action_engine();
-    } catch (const std::exception& e) {
-        rawterm::exit_alt_screen();
-        rawterm::Cursor::cursor_block();
-        spdlog::get("basic_logger")->info(e.what());
-        std::println("{}", e.what());
-    }
+    std::set_terminate(&handler);
 
-    rawterm::exit_alt_screen();
-    rawterm::Cursor::cursor_block();
+    Controller c;
+    c.create_view(flags);
+    c.start_action_engine();
+
+    exit_app();
     return 0;
 }
